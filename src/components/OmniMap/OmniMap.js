70c98fa0efcc133b61b0d5cfd8dfaea6c95de115
marke'l.spellman <marke'l.spellman@peerfitplatform.onmicrosoft.com>
2022-11-16 20:39:46 -0500

    Switch to Map in Webview - Removes use of react-native-maps - Implements expo WebView - added prettier config file

--- src/components/OmniMap/OmniMap.js
+++ src/components/OmniMap/OmniMap.js
@@ -1,86 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React from 'react'
+import { Dimensions, StyleSheet, View } from 'react-native'
+import WebView from 'react-native-webview'
 
 export default function OmniMap(location) {
-
-  const [region, setRegion] = useState({});
-  const [keyword, setKeyword] = useState({});
-  const [markers,  setMarkers] = useState([])
-  const [markersVisible, setMarkersVisible] = useState(false)
-
-  const setMapLocation = () => {
-    const region = {
-      longitude: location.location.longitude,
-      latitude: location.location.latitude,
-      longitudeDelta: 0.10,
-      latitudeDelta: 0.10
-    }
-    setKeyword('Bars')
-    setRegion(region)
-  }
-
-  const getLocalPOIs = async () => {
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${keyword}&location=${location.location.latitude}%2C${location.location.longitude}&radius=1500&key=AIzaSyC_wOKjHc9gsCIvs4JeHiG00wez5TCaY6w`
-    const { data } = await axios.get(url)
-    if (data) setMarkers(data.results)
-  }
-
-  useEffect(() => {
-    setMapLocation()
-    getLocalPOIs()
-  }, [])
-
-  const zoomInToHotspot = (coordinate) => {
-    setRegion({
-      ...coordinate,
-      longitudeDelta: 0.04,
-      latitudeDelta: 0.04
-    })
-  }
-
-  const onRegionChangeComplete = (currentRegion) => {
-    console.log(currentRegion.latitudeDelta)
-    currentRegion.latitudeDelta < 0.10 ? setMarkersVisible(true) : setMarkersVisible(false)
-    setRegion(region)
-  }
-
-  return (
-    <View style={styles.container}>
-      {region.latitude && region.longitude &&
-      <MapView 
-        showsUserLocation={true}
-        initialRegion={region}
-        provider="google"
-        style={styles.map}
-        showsPointsOfInterest={false}
-        onRegionChangeComplete={onRegionChangeComplete}
-      >
-        {markers && markersVisible && markers.map((marker, index) => (
-          <Marker
-            key={index}
-            coordinate={{latitude: marker.geometry.location.lat, longitude: marker.geometry.location.lng}}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-      </MapView>
-    }
-    </View>
-  );
+    return (
+		<View style={styles.container}>
+        <WebView
+		  		style={styles.map}
+            source={{ uri: 'https://www.google.com/maps' }}
+        />
+		</View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+})
